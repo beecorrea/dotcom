90d9e750d2e9fdc0a3ec918613d1d315fb35ae55
Bianca Correa <me@aquabianca.dev>
2026-06-28 20:39:24 -0300

    simplify notequote

--- src/components/note/NoteQuote.vue
+++ src/components/note/NoteQuote.vue
@@ -32,17 +32,4 @@
 .note-quote :deep(p:not(:last-child)) {
   margin-bottom: 0.75rem;
 }
-
-.note-quote :deep(footer) {
-  display: block;
-  font-size: 0.9rem;
-  color: var(--color-text-secondary);
-  font-style: normal;
-  text-align: right;
-  margin-top: 0.75rem;
-}
-
-.note-quote :deep(footer::before) {
-  content: "— ";
-}
 </style>
